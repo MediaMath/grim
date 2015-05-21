8edb63dcb783cd2dade42eedf2a29a0a0e7418e4
varun06 <vakumar@mediamath.com>
2015-05-21 11:18:02 -0500

    grim local test

--- grim.go
+++ grim.go
@@ -201,7 +201,7 @@ func notify(config *effectiveConfig, hook hookEvent, state refStatus, message st
 		return nil
 	}
 
-	//add grimServerID/grimQueueName to hipchat message inline
+	//add grimServerID/grimQueueName to hipchat message
 	message += "ServerID:" + config.grimServerID
 
 	ghErr := setRefStatus(config.gitHubToken, hook.owner, hook.repo, hook.statusRef, state, "", message)
