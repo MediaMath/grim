904a7598905c492c56859eb075c66d729f07434a
harisgodil-MM <hgodil@MediaMath.com>
2015-06-09 07:55:01 -0700

    fixed formatting

--- notify_test.go
+++ notify_test.go
@@ -33,7 +33,7 @@ var testHook = hookEvent{
 	EventName: "push",
 }
 
-func TestLoggingHipChatIncompleteSetup(t *testing.T){
+func TestLoggingHipChatIncompleteSetup(t *testing.T) {
 	var buf bytes.Buffer
 	logger := log.New(&buf, "", log.Lshortfile)
 
